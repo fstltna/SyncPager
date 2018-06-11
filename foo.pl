fcc86621a81a2ac58a8f6e1ab6b955f2fbdad096
fstltna <fstltna@yahoo.com>
2018-06-11 01:06:09 -0700

    Start setting up GUI module

---
+++ foo.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+# load wxPerl main module
+use Wx;
+
+# every application must create an application object
+package MyApp;
+
+use base 'Wx::App';
+
+my $VERSION = "1.0";
+
+# this method is called automatically when an application object is
+# first constructed, all application-level initialization is done here
+sub OnInit {
+
+    # create a new frame (a frame is a top level window)
+    my $frame = Wx::Frame->new( undef,           # parent window
+                                -1,              # ID -1 means any
+                                "SyncPager $VERSION",  # title
+                                [-1, -1],         # default position
+                                [250, 150],       # size
+                               );
+
+    # show the frame
+    $frame->Show( 1 );
+}
+
+package main;
+
+# create the application object, this will call OnInit
+my $app = MyApp->new;
+# process GUI events from the application this function will not
+# return until the last frame is closed
+$app->MainLoop;
+
+exit(0);
